51ee685cb76c034971129967f43cd3a1bc5fa6ab
Rawand-hub <56142655+Rawand-hub@users.noreply.github.com>
2019-10-04 10:25:27 +0200

    Update P5_10.java
    I fixed the input.close();

--- 05-Methods/P5_10.java
+++ 05-Methods/P5_10.java
@@ -7,22 +7,26 @@ percentageRaise = readDouble("What percentage raise would you like?");*/
 
 import java.util.Scanner;
 
+    
+
 public class P5_10 {
+    private static Scanner input;
+    
     public static void main(String[] args){
+        input = new Scanner(System.in);
         double salary = readDouble("Please enter your salary: ");
         double percentageRaise = readDouble("What percentage raise would you like?");
+        input.close();
         
-        System.out.printf("Salary is: %.2f", salary);
-        System.out.printf("Salary is: %.2f", percentageRaise);
+        System.out.printf("Salary is: %.2f %n", salary);
+        System.out.printf("percentageRaise is: %.2f", percentageRaise);
     }
     
     public static double readDouble(String prompt){
         System.out.println(prompt);
-        
-        Scanner input = new Scanner(System.in);
         double value = input.nextDouble();
-        input.close();
-        
         return value;
+        
+        
     }
 }
